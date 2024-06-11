af02511d45c309cd8a0fa24ee43c0d9c218b209b
fede1712 <fedemarinasruiz@gmail.com>
2024-06-11 08:24:59 +0200

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Marvel App",
+  description: "Application to get information about different Marvel characters.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="es">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
